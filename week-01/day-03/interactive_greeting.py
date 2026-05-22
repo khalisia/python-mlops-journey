aba32fb9c7ae934f62a83708d8b8f8cf8bfd1227
khalisia <luvindikhalisia@gmail.com>
2026-05-22 20:43:36 +0300

    day 3: learned input, type conversion, and f-strings

---
+++ week-01/day-03/interactive_greeting.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+goal = input("What is your career goal? ")
+x = age + 1
+language = input("What is your favorite programming language? ")
+
+print("\n---USER PROFILE---")
+print(f"Hello, {name}!")
+print(f"You are {age} years old")
+print(f"Your goal is to become a {goal}.")
+print(F"Next year you will be {x} years old")
+print(f"Your favorite programming language is {language}")
+print(name.upper())
