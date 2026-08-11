fe26cbfafa9b537dd20864ecfd2289fc2429590e
khalisia <luvindikhalisia@gmail.com>
2026-08-11 11:22:02 +0300

    Add NumPy basics and student scores analyzer

---
+++ week-05/day-31/student_scores_analyzer.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+scores = np. array([78, 85, 92, 67, 88])
+
+print("Scores:", scores)
+print("Average:", np.mean(scores))
+print("Highest:", np.max(scores))
+print("Lowest:", np.min(scores))
+print("Total:", np.sum(scores))
